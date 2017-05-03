c7c5d09359ac39c83ef28ecff226b3dc46085549
Yishin Lin <yishin.lin@utas.edu.au>
2017-05-03 21:11:31 +1000

    changet tests

--- tests/n1PDF_test.R
+++ tests/n1PDF_test.R
@@ -1,52 +1,166 @@
 rm(list=ls())
 
-data <- seq(0, 3, length.out = 1e3)
-tmp1 <- gpda::n1PDF(data, nsim=2^22)
-tmp2 <- cpda::n1PDF(data, b=1, A=.5, mean_v=c(2.4, 1.6), sd_v=c(1, 1), t0=.5, nsim=1e5, debug=F)
-tmp3 <- lba::n1PDFfixedt0(data, b=1, A=.5, mean_v=c(2.4, 1.6), sd_v=c(1, 1), t0=.5)
-tmp4 <- rtdists::n1PDF(data, b=1, A=.5, mean_v=c(2.4, 1.6), sd_v=c(1, 1), t0=.5, silent=T)
-tmp5 <- gpda::n1PDF_test(data, nsim=2^20)
+##   A    b  mean_v sd_v,    t0
+##0.07 0.09 -7.37   -4.36  0.94  0.049 
+
+data <- seq(0, 3, length.out = 1e3);
+pvec <- c(A=0.07, b=0.09, t0=.94)
+mean_v <- c(-7.37, -4.36)
+sd_v <- c(1,1)
+tmp1 <- gpda::n1PDF(data, b=pvec[2], A=pvec[1], mean_v=mean_v, sd_v=sd_v, 
+  t0=pvec[3], nsim=2^20, debug=T)
+tmp2 <- cpda::n1PDF(data, b=pvec[2], A=pvec[1], mean_v=mean_v, sd_v=sd_v, 
+  t0=pvec[3], debug=T)
+tmp3 <- clba::n1PDFfixedt0(data, b=pvec[2], A=pvec[1], mean_v=mean_v, sd_v=sd_v, t0=pvec[3])
+tmp4 <- rtdists::n1PDF(data, b=pvec[2], A=pvec[1], mean_v=mean_v, sd_v=sd_v, t0=pvec[3], silent=T)
+
+gpda::n1PDF
+tmp1 <- gpda::n1PDF(data,nsim=2^21, debug=T)
+tmp4 <- rtdists::n1PDF(data, b=1, A=.5, mean_v=c(2.4,1.6), sd_v=c(1,1), t0=.5, silent=T)
 
 
+tmp5 <- cpda::rlba_test(2^20, b=pvec[2], A=pvec[1], mean_v=mean_v, sd_v=sd_v, 
+  t0=pvec[3])
+tmp6 <- rtdists::rLBA(2^20, b=pvec[2], A=pvec[1], mean_v=mean_v, sd_v=sd_v, 
+  t0=pvec[3], silent=T)
+names(tmp6) <- c("RT","R")
+
+base::min(tmp5[tmp5$R==1,"RT"])
+base::max(tmp5[tmp5$R==1,"RT"])
+base::min(tmp6[tmp6$R==1,"RT"])
+base::max(tmp6[tmp6$R==1,"RT"])
+
+round(tmp1,2)
+round(as.vector(tmp2),2)
+round(tmp4,4)
+
+
+all.equal(as.vector(tmp1), as.vector(tmp2))
 all.equal(as.vector(tmp1), as.vector(tmp3))
 all.equal(as.vector(tmp1), as.vector(tmp4))
 all.equal(as.vector(tmp2), as.vector(tmp3))
 all.equal(as.vector(tmp2), as.vector(tmp4))
+
+round(tmp1, 2)
+round(tmp4, 3)
+
+dat1 <- gpda::rlba(2^20, b=pvec[2], A=pvec[1], mean_v=mean_v, sd_v=sd_v, 
+  t0=pvec[3], nthread=64); str(dat1)
+dat2 <- cpda::rlba_test(2^20, b=pvec[2], A=pvec[1], mean_v=mean_v, sd_v=sd_v, 
+  t0=pvec[3]); str(dat2)
+dat3 <- rtdists::rLBA(2^20, b=pvec[2], A=pvec[1], mean_v=mean_v, sd_v=sd_v,
+  t0=pvec[3], silent=T)
+names(dat3) <- c("RT","R"); str(dat3)
+
+table(dat1$R)
+## Trim ----
+sum(dat1$RT>5)
+sum(dat2$RT>5)
+sum(dat3$RT>5)
+
+dat1 <- dat1[dat1$RT < 5, ]
+dat2 <- dat2[dat2$RT < 5, ]
+dat3 <- dat3[dat3$RT < 5, ]
+
+dat1c <- dat1[dat1[,2]==1, 1]
+dat1e <- dat1[dat1[,2]==2, 1]
+dat2c <- dat2[dat2[,2]==1, 1]
+dat2e <- dat2[dat2[,2]==2, 1]
+dat3c <- dat3[dat3[,2]==1, 1]
+dat3e <- dat3[dat3[,2]==2, 1]
+
+
+den1c <- density(dat1c)
+den2c <- density(dat2c)
+den3c <- density(dat3c)
+den1e <- density(dat1e)
+den2e <- density(dat2e)
+den3e <- density(dat3e)
+
+
+lines(data,tmp1)
+lines(data,tmp4, lwd=3, col="red")
+
+plot(data,tmp4, lwd=3, col="red", type="l")
+
+par(mfrow=c(1,3))
+hist(dat1c, breaks="fd",  col="grey", freq=FALSE, xlab="RT (s)", 
+  main="GPU-Choice 1", xlim=c(0, 3)) ## gpu float
+lines(den2c, col="red",  lty="dashed",  lwd=1.5) ## cpu
+lines(den3c, col="blue", lty="dashed",  lwd=3.0) ## rtdists
+lines(den12c, col="green", lty="dashed",  lwd=4.0) ## gpu double
+
+plot(den1c$x, den1c$y, type="l", ylim=c(0, 5))
+lines(den1e$x, den1e$y)
+
+lines(den2c$x, den2c$y, col="red", lwd=2, lty="dotted")
+lines(den2e$x, den2e$y, col="red", lwd=2, lty="dotted")
+
+lines(den3c$x, den3c$y, col="blue", lwd=2, lty="dashed")
+lines(den3e$x, den3e$y, col="blue", lwd=2, lty="dashed")
+
+plot(den1c$x, den1c$y, type="l", ylim=c(0,5))
+lines(den1e$x, den1e$y)
+
+lines(den2c$x, den2c$y, col="red", lwd=2, lty="dotted")
+lines(den2e$x, den2e$y, col="red", lwd=2, lty="dotted")
+
+lines(den3c$x, den3c$y, col="blue", lwd=2, lty="dashed")
+lines(den3e$x, den3e$y, col="blue", lwd=2, lty="dashed")
+
+
+
 all.equal(as.vector(tmp3), as.vector(tmp4))
 
-all.equal(as.vector(tmp5), as.vector(tmp3))
-all.equal(as.vector(tmp5), as.vector(tmp4))
+base::min(tmp1$RT[tmp1$RT!=0])
+base::max(tmp1$RT[tmp1$RT!=0])
+base::max(tmp1$RT)
+table(tmp1$R)
+
+base::sum(tmp1$RT==0)
+base::sum(tmp1$RT)
+base::sum(tmp1$RT^2)
+
+stats::sd(tmp1$RT[tmp1$RT!=0])
+
+tmp2 <- cpda::n1PDF(data, b=1, A=.5, mean_v=c(2.4, 1.6), sd_v=c(1, 1), t0=.5, nsim=2^20, debug=F)
+tmp3 <- clba::n1PDFfixedt0(data, b=1, A=.5, mean_v=c(2.4, 1.6), sd_v=c(1, 1), t0=.5)
+tmp4 <- rtdists::n1PDF(data, b=1, A=.5, mean_v=c(2.4, 1.6), sd_v=c(1, 1), t0=.5, silent=T)
+
+
+
+
+
 
 
 ## NB1: nsim = 1e6
 ## NB2: lba:n1PDF and rtdists::n1PDF used analytic probability density function
-## NB3: gpda::n1PDF use double precision 
+## NB3: gpda::n1PDF use double precision
 ## Unit: microseconds
 ## expr                                 min         lq        mean      median         uq        max neval cld
-## lba::n1PDF(data, ...)             35.970     65.513     82.3868     87.5135     94.148    115.031    10   a  
-## rtdists::n1PDF(data, ...)        355.428    482.959    533.1549    523.8525    598.199    777.553    10   a  
+## lba::n1PDF(data, ...)             35.970     65.513     82.3868     87.5135     94.148    115.031    10   a
+## rtdists::n1PDF(data, ...)        355.428    482.959    533.1549    523.8525    598.199    777.553    10   a
 ## gpda::n1PDF_float(data, ...)  115802.444 116215.700 122231.5587 116987.1755 124510.151 145378.491    10   b
-## gpda::n1PDF(data, ...)        120642.014 121562.045 133174.1721 124230.2235 149902.235 159919.483    10   b 
+## gpda::n1PDF(data, ...)        120642.014 121562.045 133174.1721 124230.2235 149902.235 159919.483    10   b
 ## cpda::n1PDF(data, ...)        310518.881 328194.527 355060.7977 351149.1915 370555.805 451738.621    10   c
 
+n <- 2^20
+2^20
 res <- microbenchmark::microbenchmark(
-                           gpda::n1PDF(data, nsim=1e5),
-                           gpda::n1PDF(data, nsim=2^20),
-                           gpda::n1PDF_test(data, nsim=2^20),
-                           gpda::n1PDF_test(data, nsim=2^21),
+                           gpda::n1PDF(data, nsim=1048576, debug=F),
                            lba::n1PDF(data, A=0.5, b=1, mean_v=c(2.4,1.6), sd_v=c(1,1), t0=0.5),
-                           cpda::n1PDF(data, nsim=2^20, A=0.5, b=1, mean_v=c(2.4,1.6), sd_v=c(1,1),
+                           cpda::n1PDF(data, nsim=n, A=0.5, b=1, mean_v=c(2.4,1.6), sd_v=c(1,1),
                                        t0=0.5, debug=F),
                            rtdists::n1PDF(data, b=1, A=.5, mean_v=c(2.4, 1.6), sd_v=c(1, 1), t0=.5, silent=T),
                            times=10L)
-  
+
 res
 
 
 ## Test an unreasonable parameter vector ----
-tmp1 <- gpda::n1PDF(10, b=3.04, A=1.76, mean_v = c(2.79, -7.04), sd_v=c(1, 1), 
+tmp1 <- gpda::n1PDF(10, b=3.04, A=1.76, mean_v = c(2.79, -7.04), sd_v=c(1, 1),
   t0=0.35)
-tmp2 <- cpda::n1PDF(10, b=3.04, A=1.76, mean_v = c(2.79, -7.04), sd_v=c(1, 1), 
+tmp2 <- cpda::n1PDF(10, b=3.04, A=1.76, mean_v = c(2.79, -7.04), sd_v=c(1, 1),
   t0=0.35)
 round(tmp1,5)
 round(tmp2,5)
@@ -62,8 +176,8 @@ mean_v <- c(2.4, 1.6)
 sd_v <- c(1,1)
 
 
-## dat1 <- rtdists::rLBA(n, A=.5, b=1, mean_v=c(2.4, 1.6), sd_v=c(1, 1), t0=.5, silent=T); 
-dat1 <- rtdists::rLBA(n, A=A, b=b, mean_v=mean_v, sd_v=sd_v, t0=t0, silent=T); 
+## dat1 <- rtdists::rLBA(n, A=.5, b=1, mean_v=c(2.4, 1.6), sd_v=c(1, 1), t0=.5, silent=T);
+dat1 <- rtdists::rLBA(n, A=A, b=b, mean_v=mean_v, sd_v=sd_v, t0=t0, silent=T);
 names(dat1) <- c("RT","R")
 datc1 <- dat1[dat1$R==1, "RT"]
 date1 <- dat1[dat1$R==2, "RT"]
@@ -74,13 +188,13 @@ dene1 <- as.vector(lba::n1PDF(date1, A=A, b=b, mean_v=c(mean_v[2], mean_v[1]), s
 
 denc2 <- gpda::n1PDF(datc1, nsim=1e5, A=A, b=b, mean_v=mean_v, sd_v=sd_v,
   t0=t0)
-dene2 <- gpda::n1PDF(date1, nsim=1e5, A=A, b=b, mean_v=c(mean_v[2], mean_v[1]), 
+dene2 <- gpda::n1PDF(date1, nsim=1e5, A=A, b=b, mean_v=c(mean_v[2], mean_v[1]),
   sd_v=sd_v,
   t0=t0)
 
 denc3 <- cpda::n1PDF(datc1, nsim=1e5, A=A, b=b, mean_v=mean_v, sd_v=sd_v,
   t0=t0)
-dene3 <- cpda::n1PDF(date1, nsim=1e5, A=A, b=b, mean_v=c(mean_v[2], mean_v[1]), 
+dene3 <- cpda::n1PDF(date1, nsim=1e5, A=A, b=b, mean_v=c(mean_v[2], mean_v[1]),
   sd_v=sd_v,
   t0=t0)
 
@@ -104,7 +218,7 @@ points(date1, dene2, pch=2, col="blue")
 res <- microbenchmark::microbenchmark(
   lba::n1PDF(datc1, A=0.5, b=1, mean_v=c(2.4,1.6), sd_v=c(1,1), t0=0.5),
   gpda::n1PDF(datc1, nsim=1e6, A=0.5, b=1, mean_v=c(2.4,1.6), sd_v=c(1,1),
-    t0=0.5), 
+    t0=0.5),
   cpda::n1PDF(datc1, nsim=1e6, A=0.5, b=1, mean_v=c(2.4,1.6), sd_v=c(1,1),
     t0=0.5),
   times=10L)
@@ -114,6 +228,6 @@ res
 ## Test out-of-range ----
 gpda::n1PDF(10, b=1.04, A=.44, t0=.18, mean_v=c(2.2, 1.6), sd_v=c(1,1))
 
-# [b, A, t0]: [1.040 0.436 0.179 ] 
+# [b, A, t0]: [1.040 0.436 0.179 ]
 # mean_v and sd_v[0]: [2.160 1.000]
 # mean_v and sd_v[1]: [1.611 1.000]
--- tests/reduce_test.R
+++ tests/reduce_test.R
@@ -1,77 +1,106 @@
 rm(list=ls())
-## min 512 = 2^9 elements
-2^10
-dat0   <- gpda::rlba(2^11, nthread=64); str(dat0)
-result <- gpda::min(dat0$RT); result
-result <- gpda::max(dat0$RT); result
-result <- gpda::minmax(dat0$RT); result
-result <- gpda::sum(dat0$RT); result
-result <- gpda::sd(dat0$RT); result
-
-n <- 2^23
-dat0   <- gpda::rlba(n, nthread=64); str(dat0)
-result <- gpda::sqsum(dat0$RT); result
-base::sum(dat0$RT^2);
+## sum test
+dat0   <- gpda::rlba(2^20, nthread=256); str(dat0)
+result <- gpda::sum(dat0$RT, debug=T); result
+base::sum(dat0$RT)
+result <- gpda::sumur(dat0$RT, debug=T); result
 
 
-stats::sd(dat0$RT);
-base::min(dat0$RT); base::max(dat0$RT)
-base::sum(dat0$RT);
+res <- microbenchmark::microbenchmark(
+                           gpda::sum(dat0$RT, debug=FALSE),
+                           gpda::sumur(dat0$RT, debug=FALSE),
+                           base::sum(dat0$RT),
+  times=100L)
 
-
-
-dat0 <- cpda::rlba_test(2^21, b=1, A=.5, mean_v=c(2.4, 1.6), sd_v=c(1,1), t0=.5)
-str(dat0)
-
-
-
-variance <- sum((dat0$RT - mean(dat0$RT))^2) / length(dat0$RT)
-sqrt(variance); variance
-
-
-( sum(dat0$RT^2) - (sum(dat0$RT)^2) / length(dat0$RT) ) / 511
-    
-
-
-(dat0$RT - mean(dat0$RT))^2
-
-sum( (dat0$RT - mean(dat0$RT))^2 )
-
-round((dat0$RT[1:10] - mean(dat0$RT))^2, 4)
-
-head(dat0)
-
-
-
-
-
-
-res <- microbenchmark::microbenchmark(gpda::reduce0(dat0$RT),
-                               gpda::reduce1(dat0$RT),
-                               times=10L)
 res
 
-unsigned int tid = threadIdx.x;
-unsigned int i   = blockIdx.x * blockDim.x + threadIdx.x;
-unsigned int tid = threadIdx.x;
-unsigned int i   = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
 
-0*256+0:255
-1*256+0:255
+## sum test
+dat0   <- gpda::rlba(2^20, nthread=256); str(dat0)
+result <- gpda::sqsum(dat0$RT); result
+base::sum(dat0$RT^2);
+result <-  gpda::sqsumurd(dat0$RT); result
 
-0*(256*2) + 0:255
-1*(256*2) + 0:255
+res <- microbenchmark::microbenchmark(
+                           gpda::sqsum(dat0$RT, debug=FALSE),
+                           gpda::sqsumurd(dat0$RT, debug=FALSE),
+                           base::sum(dat0$RT^2),
+  times=100L)
 
-0*(256*2) + 0:255
-##1*(256/2*2) + 0:255
-
-tid <- 0:7
-nBlk <- 16
-nTh <- 8
-tmp <- matrix(numeric(nBlk*nTh), ncol=nTh)
-for(i in 0:(nBlk-1)) {
-    tmp[i+1,] <- i * nBlk + (0:(nTh-1))
-}
-tmp
+res
 
 
+
+result <- gpda::min(dat0$RT, debug=T); result
+result <- gpda::minunroll(dat0$RT,debug=T); result
+
+
+result <- gpda::min(dat0$RT, debug=T); result
+result <- gpda::max(dat0$RT); result
+result <- gpda::minmax(dat0$RT); result
+result <- gpda::sd(dat0$RT); result
+
+base::sum(dat0$RT); 
+base::min(dat0$RT); base::max(dat0$RT)
+minmax <- function(dat) {c(base::min(dat), base::max(dat))}
+minmax(dat0)
+stats::sd(dat0$RT);
+
+res <- microbenchmark::microbenchmark(
+  gpda::sd(dat0$RT, debug=FALSE),
+  sd(dat0$RT),
+  times=100L)
+
+res
+
+
+result <- gpda::n1min(dat0)
+
+
+
+
+
+
+
+
+
+
+
+
+res <- microbenchmark::microbenchmark(
+  gpda::minmax(dat0$RT, debug=FALSE),
+  findminmax(dat0$RT),
+  times=100L)
+
+res
+
+
+res <- microbenchmark::microbenchmark(
+  gpda::max(dat0$RT, debug=FALSE),
+  gpda::maxtest(dat0$RT, debug=FALSE),
+  base::max(dat0$RT),
+  times=100L)
+
+res
+
+
+
+res <- microbenchmark::microbenchmark(
+  gpda::min(dat0$RT, debug=FALSE),
+  gpda::min(dat0$RT, debug=TRUE),
+  base::min(dat0$RT),
+  times=100L)
+
+res
+
+
+res <- microbenchmark::microbenchmark(gpda::sum(dat0$RT, debug=FALSE),
+                                      base::sum(dat0$RT),
+                               times=100L)
+res
+
+res <- microbenchmark::microbenchmark(gpda::sqsum(dat0$RT, debug=FALSE),
+                                      base::sum(dat0$RT^2),
+                               times=100L)
+
+res
