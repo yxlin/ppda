7b6dace38881568eb375cf66c563ff2061dfbd2f
Yishin Lin <yishin.lin@utas.edu.au>
2017-07-01 09:26:03 +1000

    add comments for density.h

--- inst/include/density.h
+++ inst/include/density.h
@@ -1,7 +1,20 @@
-__global__ void histc_kernel(double *binedge, double *rng, unsigned int *nrng, unsigned int *out);
-__global__ void histc_kernel(float *binedge, float *rng, unsigned int *nrng, unsigned int *out);
-void histc_entry(double *binedge, double *rng, int nrng, int ngrid, unsigned int *out);
-void n1PDF(double *RT0, int *ndata, int *n, double *b, double *A, double *mean_v, int *nmean_v,
-  double *sd_v, int *nsd_v, double *t0, int *nth, bool *debug, double *out);
-void n1PDF_test(double *RT0, int *ndata, int *n, double *b, double *A, double *mean_v, int *nmean_v,
-  double *sd_v, int *nsd_v, double *t0, int *nth, bool *debug, double *out);
+// a CUDA kernel function to compute a histogram of the given random number 
+// (double rng) and defined bin edges (double binedge).   
+__global__ void histc_kernel(double *binedge, double *rng, unsigned int *nrng, 
+  unsigned int *out);
+
+// a CUDA kernel function to compute a histogram. This function takes float 
+// binedge and float rng    
+__global__ void histc_kernel(float *binedge, float *rng, unsigned int *nrng,
+  unsigned int *out);
+
+// A C function to access histc_kernel  
+void histc_entry(double *binedge, double *rng, int nrng, int ngrid, 
+  unsigned int *out);
+
+// node 1 probability density function of LBA model. node 1 refers to the 
+// density for the first accumulator. 
+void n1PDF(double *RT0, int *ndata, int *n, double *b, double *A, 
+  double *mean_v, int *nmean_v, double *sd_v, int *nsd_v, double *t0, 
+  int *nth, bool *debug, double *out);
+
